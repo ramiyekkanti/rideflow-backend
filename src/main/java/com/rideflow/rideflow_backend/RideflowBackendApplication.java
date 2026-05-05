2df40192ef9d9af0089314cf6eebd9d5fef74200
Rami Yekkanti <ramiyekkanti@gmail.com>
2026-05-05 12:55:13 +0530

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/rideflow/rideflow_backend/RideflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rideflow.rideflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rideflow/rideflow_backend/RideflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideflow.rideflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
